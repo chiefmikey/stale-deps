bc6769aa941671f949f92067ef17d58237d8f80d
Mikl Wolfe <wolfemikl@gmail.com>
2024-12-16 15:24:59 -0700

    feat(test): init configs

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+  setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
+};
